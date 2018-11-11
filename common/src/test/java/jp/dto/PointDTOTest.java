323e67f4303fe9d59799151162812102ebd621f5
Andrey <as1012923@mail.ru>
2018-11-11 17:41:41 +0700

    + pointDTOTest

---
+++ common/src/test/java/jp/dto/PointDTOTest.java
@@ -0,0 +1,16 @@
+package jp.dto;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PointDTOTest {
+    @Test
+    public void toJson() throws Exception {
+        PointDTO point = new PointDTO();
+        point.setLat(56);
+        point.setLon(74);
+        point.setAutoId("o567gfd");
+        assertTrue(point.toJson().contains("\"lat\":56"));
+    }
+}
