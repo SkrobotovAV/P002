9b935eff6f458ea39878ea7428d7ae45134d2af6
Andrey <as1012923@mail.ru>
2018-11-11 20:28:49 +0700

    преобразование из строки в объкт

--- common/src/test/java/jp/dto/PointDTOTest.java
+++ common/src/test/java/jp/dto/PointDTOTest.java
@@ -17,8 +17,17 @@ public class PointDTOTest {
         point.setLat(56);
         point.setLon(74);
         point.setAutoId("o567gfd");
+        point.setTime(System.currentTimeMillis());
         assertTrue(point.toJson().contains("\"lat\":56"));
         assertTrue(point.toJson().contains("\"time\":"));
         System.out.println(point.toJson());
+    }
+
+    @Test
+    public void decodeDTO() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        PointDTO dto = mapper.readValue(expected, PointDTO.class);
+        assertEquals(autoId, dto.getAutoId());
+        assertEquals(1541941463653L, dto.getTime());
     }
 }
