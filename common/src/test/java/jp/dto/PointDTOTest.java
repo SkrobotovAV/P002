c152c563e163b808b7541f5fc6bee708ce5a4c20
Andrey <as1012923@mail.ru>
2018-11-11 20:24:02 +0700

    преобразование объекта в json строку

--- common/src/test/java/jp/dto/PointDTOTest.java
+++ common/src/test/java/jp/dto/PointDTOTest.java
@@ -1,10 +1,16 @@
 package jp.dto;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class PointDTOTest {
+    private String expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\",\"time\":1541941463653}";
+    private String autoId = "o567gfd";
+
+
     @Test
     public void toJson() throws Exception {
         PointDTO point = new PointDTO();
@@ -12,5 +18,7 @@ public class PointDTOTest {
         point.setLon(74);
         point.setAutoId("o567gfd");
         assertTrue(point.toJson().contains("\"lat\":56"));
+        assertTrue(point.toJson().contains("\"time\":"));
+        System.out.println(point.toJson());
     }
 }
